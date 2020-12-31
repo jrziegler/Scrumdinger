d39dd982b1599737a515f349bbc2b8dbf97f47b1
João Rodrigo Ziegler <jr.ziegler@gmail.com>
2020-12-31 16:39:09 +0100

    Create the Card View

---
+++ Scrumdinger/View/CardView.swift
@@ -0,0 +1,37 @@
+//
+//  CardView.swift
+//  Scrumdinger
+//
+//  Created by Joao Rodrigo Ziegler on 31.12.20.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var scrum: DailyScrum
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(scrum.title)
+                .font(.headline)
+            Spacer()
+            HStack {
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                Spacer()
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .padding(.trailing, 20)
+            }
+            .font(.caption)
+        }
+        .padding()
+        .foregroundColor(scrum.color.accessibleFontColor)
+    }
+}
+
+struct CardView_Previews: PreviewProvider {
+    static var scrum = DailyScrum.data[0]
+    static var previews: some View {
+        CardView(scrum: scrum)
+            .background(Color.yellow)
+            .previewLayout(.fixed(width: 400, height: 60))
+    }
+}
