25b325c18c987ad402563bdadadafa53158ffcbe
João Rodrigo Ziegler <jr.ziegler@gmail.com>
2020-12-31 15:46:35 +0100

    Supplement Accessibility Data

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -11,28 +11,40 @@ struct MeetingView: View {
     var body: some View {
         VStack {
             ProgressView(value: 5, total: 15)
+
             HStack {
                 VStack(alignment: .leading) {
                     Text("Seconds Elapsed")
                         .font(.caption)
                     Label("300", systemImage: "hourglass.bottomhalf.fill")
                 }
+
                 Spacer()
+
                 VStack(alignment: .trailing) {
                     Text("Seconds Remaining")
                         .font(.caption)
+                    
                     Label("600", systemImage: "hourglass.tophalf.fill")
                 }
             }
+            .accessibilityElement(children: .ignore)
+            .accessibilityLabel(Text("Time remaining"))
+            .accessibilityValue(Text("10 minutes"))
+
             Circle()
                 .strokeBorder(lineWidth: 24, antialiased: true)
+
             HStack {
                 Text("Speaker 1 of 3.")
                     .font(.title3)
+
                 Spacer()
+
                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
                     Image(systemName: "forward.fill")
                 }
+                .accessibilityLabel(Text("Next speaker"))
             }
         }
         .padding()
