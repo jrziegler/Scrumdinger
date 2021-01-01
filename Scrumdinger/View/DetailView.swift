521c924d746a88d81d3e6a9b1fbf20d9c6d553ca
João Rodrigo Ziegler <jr.ziegler@gmail.com>
2021-01-01 13:00:11 +0100

    Present the Edit View

--- Scrumdinger/View/DetailView.swift
+++ Scrumdinger/View/DetailView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct DetailView: View {
     let scrum: DailyScrum
+    @State private var isPresented = false
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
@@ -41,7 +42,21 @@ struct DetailView: View {
             }
         }
         .listStyle(InsetGroupedListStyle())
+        .navigationBarItems(trailing: Button("Edit") {
+            isPresented = true
+        })
         .navigationTitle(scrum.title)
+        .fullScreenCover(isPresented: $isPresented) {
+            NavigationView {
+                EditView()
+                    .navigationTitle(scrum.title)
+                    .navigationBarItems(leading: Button("Cancel") {
+                        isPresented = false
+                    }, trailing: Button("Done") {
+                        isPresented = false
+                    })
+            }
+        }
     }
 }
 
