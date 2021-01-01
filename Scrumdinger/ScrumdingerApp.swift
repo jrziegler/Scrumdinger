595310c363f83e1a7f34c760b892bbeedd324385
João Rodrigo Ziegler <jr.ziegler@gmail.com>
2021-01-01 13:38:51 +0100

    Pass the Detail View a Binding to a Scrum

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -12,7 +12,7 @@ struct ScrumdingerApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                ScrumsView(scrums: DailyScrum.data)
+                ScrumsView(scrums: .constant(DailyScrum.data))
             }
         }
     }
