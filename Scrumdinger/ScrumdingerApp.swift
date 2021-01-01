bf82327b1a417f967beb3c8ccd8bdf1788e19b5f
João Rodrigo Ziegler <jr.ziegler@gmail.com>
2021-01-01 13:51:57 +0100

    Pass the List View a Binding

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -9,10 +9,12 @@ import SwiftUI
 
 @main
 struct ScrumdingerApp: App {
+    @State private var scrums = DailyScrum.data
+
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                ScrumsView(scrums: .constant(DailyScrum.data))
+                ScrumsView(scrums: $scrums)
             }
         }
     }
